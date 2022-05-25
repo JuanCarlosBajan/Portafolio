02e128699549ea09c50186321a971540c353c244
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-25 09:23:06 -0600

    routes and chakra acoplation

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard.js';
+import SideBar from './Layouts/SideBar.jsx';
+import './Assets/Styles/General.scss';
 
 function App() {
   return (
-  <></>
+    <div className="principal_div">
+      <div className="side_bar_item">
+        <SideBar />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
