f85f30743aaae37d6dcfc02daecbd6f2d52a4ed6
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-25 11:22:37 -0600

    stats

--- Frontend/src/Layouts/SideBar.jsx
+++ Frontend/src/Layouts/SideBar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 //import { Divider } from '@chakra-ui/react';
-import { FaFacebook } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import { RiInstagramFill } from 'react-icons/ri';
 import { SiGmail } from 'react-icons/si';
-import CodingStats from '../Components/CodingStats.jsx';
-import LanguageStats from '../Components/LanguageStats.jsx';
+import Stats from '../Components/Stats.jsx';
 
 export default function SideBar() {
   return (
@@ -18,21 +17,27 @@ export default function SideBar() {
       </div>
       <div className="personal_info">
         <div className="social_media">
-          <FaFacebook size={36} color="aliceblue" />
+          <FaGithub size={36} color="aliceblue" />
           <RiInstagramFill size={40} color="aliceblue" />
           <SiGmail size={40} color="aliceblue" />
         </div>
-        <div className="language_title">Idiomas</div>
+        <div className="language_title section_primary_title">Languages</div>
         <div className="language_skills">
-          <LanguageStats title="Inglés" data={85} />
-          <LanguageStats title="Español" data={100} />
+          <Stats title="English" data={85} />
+          <Stats title="Spanish" data={100} />
         </div>
-
-        <div className="coding_knowledge">
-          <CodingStats title="Python" data={90} />
-          <CodingStats title="Javascript" data={80} />
-          <CodingStats title="Java" data={70} />
-          <CodingStats title="PostgresSQL" data={50} />
+        <div className="language_title section_primary_title">
+          Coding Skills
+        </div>
+        <div className="language_skills">
+          <Stats title="JavaScript" data={80} />
+          <Stats title="SCSS" data={80} />
+          <Stats title="React" data={85} />
+        </div>
+        <div className="language_skills">
+          <Stats title="Python" data={90} />
+          <Stats title="PostgresSQL" data={70} />
+          <Stats title="Unity" data={75} />
         </div>
       </div>
     </div>
