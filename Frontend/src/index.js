bd80c197c5ba9a4f10834475a9b55616cd428f36
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-29 21:40:04 -0600

    configs

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  ChakraProvider,
-  extendTheme,
-  withDefaultColorScheme,
-} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js';
-
-const customTheme = extendTheme(
-  {
-    colors: {
-      brand: '#FFF246',
-    },
-    components: {
-      Alert: {
-        defaultProps: {
-          colorScheme: 'blue',
-        },
-      },
-    },
-  },
-  withDefaultColorScheme({ colorScheme: 'brand' })
-);
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
-  <ChakraProvider theme={customTheme}>
+  <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index_bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -83,6 +84,9 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
