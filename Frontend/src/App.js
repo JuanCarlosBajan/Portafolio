fef8ed52dc321d339a8bfbd2e8287e05587ccb4f
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-29 21:36:48 -0600

    Rutas

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard.js';
+import Games from './Pages/Games.js';
+import Apps from './Pages/Apps.js';
+import Web from './Pages/Web.js';
+import Me from './Pages/Me.js';
 import SideBar from './Layouts/SideBar.jsx';
 import './Assets/Styles/General.scss';
 
@@ -12,7 +16,11 @@ function App() {
       </div>
       <div className="content_item">
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/Games" element={<Games />} />
+          <Route path="/Apps" element={<Apps />} />
+          <Route path="/Web" element={<Web />} />
+          <Route path="/Me" element={<Me />} />
         </Routes>
       </div>
     </div>
