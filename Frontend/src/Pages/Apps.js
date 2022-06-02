e9b94e787581e0f3f0c5888b4257cb3f876c93b7
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-06-01 20:07:57 -0600

    app page

--- Frontend/src/Pages/Apps.js
+++ Frontend/src/Pages/Apps.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
+import CYL1 from '../Assets/Images/CheckYourLifeApp/1.PNG';
+import CYL2 from '../Assets/Images/CheckYourLifeApp/2.PNG';
+import CYL3 from '../Assets/Images/CheckYourLifeApp/3.PNG';
 import Element from '../Components/Element.jsx';
 
 export default function Apps() {
   const projects = [
     {
       title: 'Check Your Life',
-      imgs: [],
-      github: '',
+      imgs: [CYL1, CYL2, CYL3],
+      github: ['https://github.com/MarianoReyes/CheckYourLife'],
       description:
         'This project aims to provide all the necessary tools for each user to manage their personal resources (Money, Time, Goals and Tasks) in the best way.',
       tecnologies: [
@@ -48,10 +51,11 @@ export default function Apps() {
       {projects.map((project) => (
         <Element
           key={project.title}
-          //img={project.imgs[0]}
+          imgs={project.imgs}
           title={project.title}
           description={project.description}
           tecnologies={project.tecnologies}
+          github={project.github}
         />
       ))}
     </div>
